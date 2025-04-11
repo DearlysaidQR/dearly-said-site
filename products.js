62e1fe2e5024852f39ffe402cd5a68e6eff7f7be
DearlysaidQR <dearlysaidqrmessages@gmail.com>
2025-04-11 19:56:54 -0400

    products.js

---
+++ products.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    id: 1,
+    name: "Happy Birthday Card",
+    category: "Birthday",
+    price: 5.99,
+    image: "images/birthday1.jpg",
+  },
+  {
+    id: 2,
+    name: "Anniversary Love Card",
+    category: "Anniversary",
+    price: 6.99,
+    image: "images/anniversary1.jpg",
+  },
+  {
+    id: 3,
+    name: "Thank You QR Card",
+    category: "Thank You",
+    price: 4.99,
+    image: "images/thankyou1.jpg",
+  },
+  {
+    id: 4,
+    name: "Sympathy QR Message",
+    category: "Sympathy",
+    price: 5.49,
+    image: "images/sympathy1.jpg",
+  },
+  {
+    id: 5,
+    name: "Custom Voice Message Card",
+    category: "Custom",
+    price: 7.99,
+    image: "images/custom1.jpg",
+  }
+];
